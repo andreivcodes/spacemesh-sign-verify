38110b5e71c4e9d9b7fe3172144524570410a48d
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2022-09-22 13:32:46 +0300

    it works

---
+++ pages/api/verify.ts
@@ -0,0 +1,38 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {
+  fromHexString,
+  toHexString,
+  verifyMessage,
+} from "@andreivcodes/spacemeshlib";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { text, signature, publicKey } = req.query;
+
+  console.log("=================================");
+  console.log(text);
+  console.log(signature);
+  console.log("0x" + String(publicKey).slice(26));
+
+  const verify = await verifyMessage(
+    String(publicKey).slice(2),
+    String(text),
+    String(signature).slice(2)
+  );
+
+  let result = {
+    name: "Signature Verification Result",
+    result: Boolean(verify),
+  };
+
+  console.log(result);
+  console.log("=================================");
+  res.status(200).json(result);
+}
