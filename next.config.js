a57838153d427837f03d22b889a36aa735aaef74
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2022-09-22 13:53:33 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  webpack: (config, options) => {
+    config.experiments = {
+      layers: true,
+      asyncWebAssembly: true,
+    };
+    return config;
+  },
+};
